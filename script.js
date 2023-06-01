1daf040d08e7ee20a58ff6f6c851b48ac556054d
Dead4an <linker4an@mail.ru>
2023-06-01 21:47:20 +0300

    final

--- script.js
+++ script.js
@@ -39,12 +39,31 @@ class Board {
   }
 
   showFields() {
+    const gameBoard = document.getElementById("game-board");
+    while (gameBoard.lastChild) {
+      gameBoard.removeChild(gameBoard.lastChild);
+    }
+
+    this.fields = this.fields.sort((a, b) => {
+      return a.x - b.x || a.y - b.y;
+    });
+    console.log(this.fields);
     this.fields.forEach((field) => {
       const fieldNode = document.createElement("div");
-      fieldNode.innerHTML = field.number;
+
+      // Style
+      fieldNode.innerHTML = field.type == "empty" ? "" : field.number;
       fieldNode.classList.add(`${field.type}-field`);
       fieldNode.style.width = `${100 / this.n_square - 2}%`;
       fieldNode.style.height = `${100 / this.n_square - 2}%`;
+
+      // Events
+      if (field.type == "regular") {
+        fieldNode.addEventListener("click", field.chooseField);
+      } else {
+        fieldNode.addEventListener("click", field.replace);
+      }
+
       htmlBoard.appendChild(fieldNode);
     });
   }
@@ -57,6 +76,107 @@ class Field {
     this.x = x;
     this.y = y;
   }
+
+  chooseField() {
+    const fields = document.querySelectorAll(".regular-field, .chosen-field");
+    fields.forEach((field) => {
+      field.classList.replace("chosen-field", "regular-field");
+    });
+
+    if (this.classList.contains("empty-field")) {
+      return;
+    }
+
+    this.classList.replace("regular-field", "chosen-field");
+
+    // const id = this.innerHTML;
+    // let chosenField;
+
+    // board.fields.forEach((field, index) => {
+    //   if (field.number == id) {
+    //     chosenField = board.fields[index];
+    //   }
+    // });
+
+    // const chosenX = chosenField.x;
+    // const chosenY = chosenField.y;
+
+    // board.fields.forEach((field) => {
+    //   if (
+    //     checkDistanse(chosenField.x, chosenField.y, field.x, field.y) &
+    //     (field.type == "empty")
+    //   ) {
+    //     chosenField.x = field.x;
+    //     chosenField.y = field.y;
+    //     field.x = chosenField.x;
+    //     field.y = chosenField.y;
+    //   }
+    // });
+  }
+
+  replace() {
+    const emptyIndex = board.fields.findIndex((field) => field.type == "empty");
+    const chosenNumber = document.querySelector(".chosen-field").innerHTML;
+    const chosenIndex = board.fields.findIndex(
+      (field) => (field.number == chosenNumber) & (field.type == "regular")
+    );
+
+    const emptyField = board.fields[emptyIndex];
+    const chosenField = board.fields[chosenIndex];
+    const emptyX = emptyField.x;
+    const emptyY = emptyField.y;
+
+    if (!checkDistanse(chosenField.x, chosenField.y, emptyX, emptyY)) {
+      console.log("too far");
+      return;
+    }
+
+    const gameBoard = document.querySelector("#game-board");
+    const chosenNode =
+      gameBoard.childNodes[board.n_square * chosenField.y + chosenField.x];
+    const emptyNode = document.querySelector(".empty-field");
+
+    let chosenCoords = chosenNode.getBoundingClientRect();
+    let emptyCoords = emptyNode.getBoundingClientRect();
+
+    emptyField.x = chosenField.x;
+    emptyField.y = chosenField.y;
+    chosenField.x = emptyX;
+    chosenField.y = emptyY;
+
+    board.showFields();
+  }
+
+  // moveField(fields, x, y) {
+  //   if (checkDistanse(this.x, this.y, x, y)) {
+  //     console.log("too far");
+  //     return;
+  //   }
+
+  //   fields.forEach((field) => {
+  //     if ((field.x == x) & (field.y == y)) {
+  //       field.x = this.x;
+  //       field.y = this.y;
+  //     }
+  //   });
+
+  //   this.x = x;
+  //   this.y = y;
+  // }
+}
+
+function checkDistanse(x1, y1, x2, y2) {
+  const xDifference = Math.abs(x2 - x1);
+  const yDifference = Math.abs(y2 - y1);
+  if (
+    (xDifference > 1) |
+    (yDifference > 1) |
+    ((xDifference == 1) & (yDifference == 1))
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 function getRandomInt(min, max) {
@@ -65,6 +185,6 @@ function getRandomInt(min, max) {
 
 // Init game
 const htmlBoard = document.getElementById("game-board");
-const board = new Board(4);
+const board = new Board(3);
 board.initFields();
 board.showFields();
